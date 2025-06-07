f78323928f5591f7bbbd86d137d50c363a0c9704
atcsol <atcsol@atcsol>
2025-06-07 10:49:11 +0200

    commit

---
+++ uah_photsat_freertos_cpp/Debug/service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.d
@@ -0,0 +1,14 @@
+service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.o: \
+ ../service_libraries/pus_services/pus_sys_data_pool/src/pus_sys_data_pool.c \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/config.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/asw/edroom_glue/include/edroom_glue/edroomdf.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/basic_types.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/pus_services/pus_sys_data_pool/include/public/sys_data_pool_pids.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/serialize/include/public/serialize.h
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/pus_services/pus_sys_data_pool/include/public/pus_sys_data_pool.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/config.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/asw/edroom_glue/include/edroom_glue/edroomdf.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/basic_types.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/pus_services/pus_sys_data_pool/include/public/sys_data_pool_pids.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/serialize/include/public/serialize.h:
---
+++ uah_photsat_freertos_cpp/Debug/service_libraries/serialize/src/serialize.d
@@ -0,0 +1,10 @@
+service_libraries/serialize/src/serialize.o: \
+ ../service_libraries/serialize/src/serialize.c \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/serialize/include/public/serialize.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/config.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/asw/edroom_glue/include/edroom_glue/edroomdf.h \
+ /home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/basic_types.h
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/service_libraries/serialize/include/public/serialize.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/config.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/asw/edroom_glue/include/edroom_glue/edroomdf.h:
+/home/atcsol/git/UAH_PhotSat/uah_photsat_freertos_cpp/llsw/config/include/public/basic_types.h:
